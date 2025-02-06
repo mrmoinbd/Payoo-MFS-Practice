8efdae9ea2e9d6cfd403cece2ba1346df0cde874
Moin Uddin <moinuddin.siu.bd@gmail.com>
2025-02-07 00:10:38 +0600

    added features toggle button functionality

--- js/featurs.js
+++ js/featurs.js
@@ -1,1 +1,11 @@
-console.log('test features')
+document.getElementById('btn-show-cashmoney').addEventListener('click', function(){
+    showSectionShowById('cashout-form');
+});
+
+document.getElementById('btn-show-addmoney').addEventListener('click', function(){
+    showSectionShowById('addmoney-form');
+});
+
+document.getElementById('btn-show-transection').addEventListener('click', function(){
+    showSectionShowById('transection-section');
+});
--- js/utility.js
+++ js/utility.js
@@ -9,3 +9,12 @@ function getTextFieldById(id){
     const textValueNumber = parseFloat(textValue);
     return textValueNumber;
 };
+
+// add features toggle butoon function
+function showSectionShowById(id){
+    document.getElementById('addmoney-form').classList.add('hidden');
+    document.getElementById('cashout-form').classList.add('hidden');
+    document.getElementById('transection-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden')
+}
